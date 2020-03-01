e636cf62e1932e4931182ed1d5c8093718773782
tmorenobernal <tmorenobernal@gmail.com>
2020-03-01 17:41:41 +0100

    Added test for first requirement

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,31 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GildedRoseTest {
+public class GildedRoseTest {
 
     @Test
-    void foo() {
+    public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
+
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+
+        assertThat(app.items[0].name, is("foo"));
     }
 
+    @Test
+    public void when_sell_date_is_not_passed_then_quality_should_decrease_by_one() {
+        Item[] items = new Item[] { new Item("foo", 10, 5) };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(4));
+    }
 }
